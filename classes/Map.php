bac8b27eb409f0fb8d5dfabf0c9e405a6076fff4
Vitaliy Shapovalov <xflash55@yandex.ru>
2020-03-25 16:37:25 +0300

    add style for items on the map

--- classes/Map.php
+++ classes/Map.php
@@ -13,8 +13,8 @@ class Map
     {
         return $this->errors;
     }
-
     public function loadNewLevel($loadedNewMap = null)
+
     {
         if ($loadedNewMap == null) {
             $this->instanceOfMap = json_decode(file_get_contents('maps/map' . $this->numberOfLevelMap . '.json'));
@@ -29,7 +29,22 @@ class Map
             foreach ($verticalColumn as $cell) {
                 if ($cell = 'X') {
                     $output .= '<button type="button" disabled class="btn btn-secondary board">&nbsp;</button>';
-
+                } else if ($cell == 'P'){
+                    $output .= '<button type="button" disabled class="btn btn-primary board"><span class="fas fa-user"></span></button>';
+                } else if ($cell ==  'E'){
+                    $output .= '<button type="button" disabled class="btn btn-danger board"><span class="fas fa-dragon"></span></button>';
+                } else if ($cell == 'D'){
+                    if($this->collected_all_coins == true){
+                        $output .= '<button type="button" disabled class="btn btn-success board"><span class="fas fa-door-open"></span></button>';
+                    } else {
+                        $output .= '<button type="button" disabled class="btn btn-secondary board"><span class="fas fa-door-closed"></span></button>'
+                    }
+                } else if ($cell == 'O'){
+                    $output .= '<button type="button" disabled class="btn btn-danger board">&nbsp;</button>';
+                } else if ($cell == 'C'){
+                    $output .= '<button type="button" disabled class="btn btn-warning board">&nbsp;</button>';
+                } else {
+                    $output .= '<button type="button" disabled class="btn btn-light board">&nbsp;</button>';
                 }
             }
         }

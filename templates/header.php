4d194c1fcdd9d60a8255810eb9aa2403561a330a
Vitaliy Shapovalov <xflash55@yandex.ru>
2020-08-31 11:42:24 +0300

    fix: css interface elements'

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Game of Danger Dragon</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/map.css">
 </head>
